b143788cdf3984a1e8e43ae487745d091d020106
kai <z85385637@gmail.com>
2026-04-25 18:42:50 +0800

    exercise(W01): fizzbuzz with if/else (and match version explored)

---
+++ exercises/week-01/fizzbuzz/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    for i in 1..=100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            print!("FizzBuzz ");
+        } else if i % 5 == 0 {
+            print!("Buzz ");
+        } else if i % 3 == 0 {
+            print!("Fizz ");
+        } else {
+            print!("{i} ");
+        }
+    }
+}
